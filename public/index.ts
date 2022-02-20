1e0c7996b0c860a0d6f973c9e4842e81a9eabb34
The Larry <26318510+0xlarry@users.noreply.github.com>
2022-02-20 18:50:29 +0000

    update 4

---
+++ public/index.ts
@@ -0,0 +1,50 @@
+const aboutToggler = document.getElementById("aboutToggler") as HTMLSpanElement;
+const aboutImg = document.getElementById("aboutImg") as HTMLImageElement;
+const aboutTab = document.getElementById("aboutTab") as HTMLDivElement;
+const validatorToggler = document.getElementById("validatorToggler") as HTMLSpanElement;
+const validatorLeftImg = document.getElementById("validatorLeftImg") as HTMLImageElement;
+const validatorRightImg = document.getElementById("validatorRightImg") as HTMLImageElement;
+const validatorTab = document.getElementById("validatorTab") as HTMLDivElement;
+
+let currentTab: "about" | "validator" = "about";
+
+aboutToggler.addEventListener("click", () => {
+  if (currentTab === "validator") {
+    currentTab = "about";
+    aboutToggler.classList.remove("selectable");
+    aboutImg.style.display = "block";
+    aboutTab.style.display = "block";
+    validatorToggler.classList.add("selectable");
+    validatorLeftImg.style.display = "none";
+    validatorRightImg.style.display = "none";
+    validatorTab.style.display = "none";
+  }
+});
+
+validatorToggler.addEventListener("click", () => {
+  if (currentTab === "about") {
+    currentTab = "validator";
+    aboutToggler.classList.add("selectable");
+    aboutImg.style.display = "none";
+    aboutTab.style.display = "none";
+    validatorToggler.classList.remove("selectable");
+    validatorLeftImg.style.display = "block";
+    validatorRightImg.style.display = "block";
+    validatorTab.style.display = "block";
+  }
+});
+
+const lunaStakeBtn = document.getElementById("lunaStakeBtn") as HTMLDivElement;
+const osmoStakeBtn = document.getElementById("osmoStakeBtn") as HTMLDivElement;
+
+lunaStakeBtn.addEventListener("click", () => {
+  window.open(
+    "https://station.terra.money/validator/terravaloper1d3fv2cjukt0e6lrzd8d857jatlkht7wcp85zar"
+  );
+});
+
+osmoStakeBtn.addEventListener("click", () => {
+  window.open(
+    "https://wallet.keplr.app/#/osmosis/stake?modal=detail&validator=osmovaloper1zlymlax05tg9km9jyw496jx60v86m454a3xf3m"
+  );
+});
